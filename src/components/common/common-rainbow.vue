a0119f3c0fc2cbd5399b98389b9e5fd185965c54
妖雨纯 <876451336@qq.com>
2024-03-08 19:10:52 +0800

    ✨ feat(plugins): 心中彩虹背景组件

---
+++ src/components/common/common-rainbow.vue
@@ -0,0 +1,28 @@
+<script lang="tsx">
+import { defineComponent, onMounted } from 'vue'
+import { useCurrentElement } from '@vueuse/core'
+import { createRainbowr } from '@/plugins/rainbow/rainbow.min'
+
+export default defineComponent({
+    name: 'CommonRainbow',
+    setup(props) {
+        const element = useCurrentElement()
+
+        onMounted(() => {
+            createRainbowr(element.value)
+        })
+        return () => <div class="common-rainbow"></div>
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.common-rainbow {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    overflow: hidden;
+}
+</style>
