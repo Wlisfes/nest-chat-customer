44bbe2d2340235cd62ffbe1bd09aa037a4db4366
妖雨纯 <876451336@qq.com>
2024-03-31 15:22:01 +0800

    🎈 perf(build): 修改dev、build指令

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import { createBootstrap } from '@/utils/utils-component'
 async function bootstrap() {
     const { app } = createBootstrap()
     app.mount('#app')
+
+    console.log(import.meta.env)
 }
 bootstrap()
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 import { DefineComponent, VNode } from 'vue'
 import { useMessage, useLoadingBar, useNotification, useDialog } from 'naive-ui'
+import { AxiosInstance, InternalAxiosRequestConfig, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 declare module '*.vue' {
     const Component: DefineComponent<{}, {}, any>
@@ -10,6 +11,11 @@ declare module '*.vue' {
 declare global {
     type Refer<K = string, T = any> = Record<K, T>
     type Omix<T = Refer> = T & Refer
+
+    interface Request extends AxiosInstance {
+        <T = any, R = AxiosResponse<T>, D = any>(config: AxiosRequestConfig<D>): Promise<R & Response<T>>
+        <T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R & Response<T>>
+    }
 
     interface Window {
         $message: ReturnType<typeof useMessage>
