12cfad3a5a45bab4dfc6df079df60a5050b9366c
妖雨纯 <876451336@qq.com>
2024-04-30 20:12:45 +0800

    ✨ feat(chat): 完善用户状态、昵称修改

---
+++ src/components/common/common-revise.vue
@@ -0,0 +1,116 @@
+<script lang="tsx">
+import { defineComponent, PropType } from 'vue'
+import { useState } from '@/hooks/hook-state'
+import { stop } from '@/utils/utils-common'
+
+export default defineComponent({
+    name: 'CommonRevise',
+    emits: ['submit'],
+    props: {
+        content: { type: String as PropType<string>, default: '' },
+        label: { type: String as PropType<string> },
+        placeholder: { type: String as PropType<string> }
+    },
+    setup(props, { emit }) {
+        const { state, setState } = useState({
+            loading: false,
+            disabled: true,
+            content: props.content ?? ''
+        })
+
+        async function fetchUpdate() {
+            if (state.disabled) {
+                return await setState({ disabled: false })
+            } else if (!state.loading) {
+                return await setState({ loading: true, disabled: true }).then(() => {
+                    emit('submit', { ...state, done: setState })
+                })
+            }
+        }
+
+        return () => (
+            <n-form-item class={{ 'common-revise': true, 'chunk-start': !state.disabled || state.loading }} label={props.label}>
+                <n-input
+                    disabled={state.disabled || state.loading}
+                    placeholder={props.placeholder}
+                    type="textarea"
+                    maxlength={32}
+                    autosize={{ minRows: 1, maxRows: 3 }}
+                    v-model:value={state.content}
+                    v-slots={{
+                        suffix: () => (
+                            <n-button
+                                disabled={state.loading || !state.content}
+                                text
+                                focusable={false}
+                                onClick={(evt: Event) => stop(evt, fetchUpdate)}
+                            >
+                                {state.loading ? (
+                                    <common-loadiner size={22} size-iner={3}></common-loadiner>
+                                ) : state.disabled ? (
+                                    <n-icon size={24} color="var(--text-color-2)" component={<Iv-BsEdit />}></n-icon>
+                                ) : (
+                                    <n-icon size={26} color="var(--text-color-2)" component={<Iv-BsCheck />}></n-icon>
+                                )}
+                            </n-button>
+                        )
+                    }}
+                ></n-input>
+            </n-form-item>
+        )
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.common-revise {
+    --n-label-height: 20px;
+    --n-blank-height: 20px;
+    padding: 0 24px;
+    background-color: var(--layer-resolver-form-item);
+    transition: background-color 0.3s var(--cubic-bezier-ease-in-out);
+    &.chunk-start {
+        :deep(.n-input) {
+            border-bottom-color: var(--divider-color);
+        }
+    }
+    :deep(.n-form-item-label) {
+        font-size: 14px;
+        line-height: 24px;
+        color: var(--primary-color);
+        display: flex;
+        align-items: center;
+        padding-bottom: 5px;
+        padding-left: 0;
+    }
+    :deep(.n-input__suffix) {
+        position: absolute;
+        margin: 0;
+        right: 0;
+        top: 0;
+    }
+    :deep(.n-input) {
+        --n-border: none;
+        --n-border-disabled: none;
+        --n-border-hover: none;
+        --n-border-focus: none;
+        --n-box-shadow-focus: none;
+        --n-color: transparent;
+        --n-padding-left: 0;
+        --n-padding-right: 40px;
+        --n-color-focus: transparent;
+        --n-color-disabled: transparent;
+        --n-padding-vertical: 0;
+        --n-border-radius: 0;
+        --n-line-height-textarea: 28px;
+        --n-text-color-disabled: var(--text-color-2);
+        font-size: 16px;
+        padding-bottom: 5px;
+        border-bottom: 2px solid #0000;
+        transition: border-bottom-color 0.3s var(--cubic-bezier-ease-in-out);
+        &.n-input--focus {
+            border-bottom-color: var(--primary-color);
+        }
+    }
+}
+</style>
--- src/components/layer/layer-resolver.vue
+++ src/components/layer/layer-resolver.vue
@@ -17,23 +17,23 @@ export default defineComponent({
     },
     setup(props, { emit }) {
         const user = useUser()
-        const { element, chunkContent, divineLayerUnmounted } = useDrawer()
-        const { form, visible, setVisible } = useFormCustomize({
-            form: {
-                nickname: user.nickname,
-                comment: user.comment
-            }
-        })
+        const { visible, element, chunkContent, fetchState, divineLayerUnmounted } = useDrawer()
+        // const { form, visible, setVisible } = useFormCustomize({
+        //     form: {
+        //         nickname: user.nickname,
+        //         comment: user.comment
+        //     }
+        // })
 
         onMounted(async () => {
-            setVisible(true)
+            await fetchState({ visible: true })
             return await divineLayerUnmounted(props.observer, () => {
-                return setVisible(false)
+                return fetchState({ visible: false })
             })
         })
 
-        /**用户基础信息更新**/
-        async function fetchUserUpdate(scope: Omix<{ done: Function } & env.RestStreamUploader>) {
+        /**更新头像**/
+        async function fetchUserUpdateAvatar(scope: Omix<{ done: Function } & env.RestStreamUploader>) {
             try {
                 return await api.httpUserUpdate({ fileId: scope.fileId }).then(async ({ message }) => {
                     await user.fetchUserResolver()
@@ -43,6 +43,22 @@ export default defineComponent({
             } catch (e) {
                 return await divineNotice({ type: 'error', title: e.message }).then(async () => {
                     return await scope.done({ loading: false })
+                })
+            }
+        }
+
+        /**更新昵称、状态**/
+        async function fetchUserUpdate({ done, comment, nickname }: Omix<{ done: Function; nickname?: string; comment?: string }>) {
+            try {
+                return await api.httpUserUpdate({ nickname, comment }).then(async ({ message }) => {
+                    await user.fetchUserResolver()
+                    await divineNotice({ type: 'success', title: message })
+                    return await done({ loading: false })
+                })
+            } catch (e) {
+                console.log(e)
+                return await divineNotice({ type: 'error', title: e.message }).then(async () => {
+                    return await done({ loading: false })
                 })
             }
         }
@@ -60,206 +76,25 @@ export default defineComponent({
                 on-after-leave={() => emit('close')}
             >
                 <n-element class="layer-resolver n-chunk n-column n-auto n-disover">
-                    <chat-header title="个人信息" onClose={(evt: Event) => setVisible(false)}></chat-header>
+                    <chat-header title="个人信息" onClose={(evt: Event) => fetchState({ visible: false })}></chat-header>
                     <div class="n-chunk n-center n-middle" style={{ padding: '24px' }}>
-                        <chat-avatar upload={true} src={user.avatar} size={200} radius={100} onSubmit={fetchUserUpdate}></chat-avatar>
+                        <chat-avatar upload={true} src={user.avatar} size={200} radius={100} onSubmit={fetchUserUpdateAvatar}></chat-avatar>
                     </div>
+                    <common-revise
+                        label="昵称"
+                        placeholder="昵称"
+                        content={user.nickname}
+                        onSubmit={({ done, content }: Omix) => fetchUserUpdate({ done, nickname: content })}
+                    ></common-revise>
+                    <common-revise
+                        label="状态"
+                        placeholder="状态"
+                        content={user.comment}
+                        onSubmit={({ done, content }: Omix) => fetchUserUpdate({ done, comment: content })}
+                    ></common-revise>
                 </n-element>
             </n-drawer>
         )
     }
 })
 </script>
-
-<!-- <script lang="tsx">
-import { defineComponent, onMounted, PropType } from 'vue'
-import { useUser } from '@/store'
-import { useFormCustomize } from '@/hooks/hook-customize'
-import { stop, divineHandler, divineDelay } from '@/utils/utils-common'
-
-export default defineComponent({
-    name: 'LayerResolver',
-    emits: ['close', 'submit'],
-    props: {
-        element: { type: Object as PropType<HTMLElement> },
-        width: { type: [Number, String], default: 420 }
-    },
-    setup(props, { emit }) {
-        const user = useUser()
-
-        const { form, visible, setVisible } = useFormCustomize({
-            form: {
-                nickname: user.nickname,
-                comment: user.comment
-            }
-        })
-
-        onMounted(() => {
-            return setVisible(true)
-        })
-
-        /**回车事件拦截**/
-        function onKeydownHandler(evt: KeyboardEvent) {
-            return divineHandler(evt.key === 'Enter', {
-                handler: () => {
-                    return stop(evt, async () => {
-                        console.log(evt)
-                    })
-                }
-            })
-        }
-
-        async function onNicknameUpdate(scope: Omix, done: Function) {
-            if (!scope.disabled) {
-                return await done({ disabled: true })
-            } else if (!scope.loading) {
-                await done({ loading: true })
-                return await divineDelay(1500, () => done({ loading: false, disabled: false }))
-            }
-        }
-
-        async function onCommentUpdate(scope: Omix, done: Function) {
-            if (!scope.disabled) {
-                return await done({ disabled: true })
-            } else if (!scope.loading) {
-                await done({ loading: true })
-                return await divineDelay(1500, () => done({ loading: false, disabled: false }))
-            }
-        }
-
-        return () => (
-            <n-drawer
-                v-model:show={visible.value}
-                width={props.width}
-                to={props.element ?? document.body}
-                content-style={{ overflow: 'hidden', userSelect: 'none' }}
-                placement="left"
-                auto-focus={false}
-                mask-closable={false}
-                show-mask={false}
-                on-after-leave={() => emit('close')}
-            >
-                <n-element class="layer-resolver n-chunk n-column">
-                    <chat-header title="个人信息" onClose={(evt: Event) => setVisible(false)}></chat-header>
-                    <chat-avatar url={user.avatar} style={{ padding: '32px' }}></chat-avatar>
-                    <div style={{ flex: 1, overflow: 'hidden' }}>
-                        <n-scrollbar class="is-customize" trigger="none" size={60}>
-                            <n-form size="large" label-placement="top" show-feedback={false}>
-                                <n-form-item label="昵称">
-                                    <common-state
-                                        data-render={(scope: Omix, done: Function) => (
-                                            <n-input
-                                                readonly={scope.loading || !scope.disabled}
-                                                type="textarea"
-                                                maxlength={32}
-                                                autosize={{ minRows: 1, maxRows: 3 }}
-                                                onKeydown={onKeydownHandler}
-                                                v-model:value={form.value.nickname}
-                                                v-slots={{
-                                                    suffix: () => (
-                                                        <n-button
-                                                            disabled={scope.loading}
-                                                            text
-                                                            focusable={false}
-                                                            onClick={(evt: Event) => onNicknameUpdate(scope, done)}
-                                                        >
-                                                            {scope.loading ? (
-                                                                <common-loadiner size={22} size-iner={3}></common-loadiner>
-                                                            ) : scope.disabled ? (
-                                                                <n-icon size={28} component={<Iv-BsCheck />}></n-icon>
-                                                            ) : (
-                                                                <n-icon size={28} component={<Iv-BsEdit />}></n-icon>
-                                                            )}
-                                                        </n-button>
-                                                    )
-                                                }}
-                                            />
-                                        )}
-                                    ></common-state>
-                                </n-form-item>
-                                <n-form-item label="状态">
-                                    <common-state
-                                        data-render={(scope: Omix, done: Function) => (
-                                            <n-input
-                                                readonly={scope.loading || !scope.disabled}
-                                                type="textarea"
-                                                maxlength={32}
-                                                autosize={{ minRows: 1, maxRows: 3 }}
-                                                onKeydown={onKeydownHandler}
-                                                v-model:value={form.value.comment}
-                                                v-slots={{
-                                                    suffix: () => (
-                                                        <n-button
-                                                            disabled={scope.loading}
-                                                            text
-                                                            focusable={false}
-                                                            onClick={(evt: Event) => onCommentUpdate(scope, done)}
-                                                        >
-                                                            {scope.loading ? (
-                                                                <common-loadiner size={22} size-iner={3}></common-loadiner>
-                                                            ) : scope.disabled ? (
-                                                                <n-icon size={28} component={<Iv-BsCheck />}></n-icon>
-                                                            ) : (
-                                                                <n-icon size={28} component={<Iv-BsEdit />}></n-icon>
-                                                            )}
-                                                        </n-button>
-                                                    )
-                                                }}
-                                            />
-                                        )}
-                                    ></common-state>
-                                </n-form-item>
-                            </n-form>
-                        </n-scrollbar>
-                    </div>
-                </n-element>
-            </n-drawer>
-        )
-    }
-})
-</script>
-
-<style lang="scss" scoped>
-.layer-resolver {
-    height: 100%;
-    overflow: hidden;
-    background-color: var(--layer-common-color);
-    transition: background-color 0.3s var(--cubic-bezier-ease-in-out);
-    :deep(.n-form-item) {
-        --n-label-height: 20px;
-        --n-blank-height: 20px;
-        padding: 14px 24px 10px;
-        margin-bottom: 32px;
-        background-color: var(--layer-resolver-form-item);
-        transition: background-color 0.3s var(--cubic-bezier-ease-in-out);
-        .n-form-item-label {
-            font-size: 14px;
-            line-height: 20px;
-            display: flex;
-            align-items: center;
-            padding-bottom: 10px;
-            padding-left: 0;
-        }
-    }
-    :deep(.n-input) {
-        --n-border: none;
-        --n-border-disabled: none;
-        --n-border-hover: none;
-        --n-border-focus: none;
-        --n-box-shadow-focus: none;
-        --n-color: transparent;
-        --n-padding-left: 0;
-        --n-padding-right: 40px;
-        --n-color-focus: transparent;
-        --n-padding-vertical: 0;
-        --n-line-height-textarea: 28px;
-        font-size: 16px;
-        .n-input__suffix {
-            position: absolute;
-            margin: 0;
-            right: 0;
-            top: 0;
-        }
-    }
-}
-</style> -->
