cde2c407e41f5439fd79b08f8a0ed489d4d4758c
妖雨纯 <876451336@qq.com>
2024-05-12 20:53:28 +0800

    ✨ feat(next): 临时存储

---
+++ src/components/next/next-join-communit.vue
@@ -0,0 +1,17 @@
+<script lang="tsx">
+import { defineComponent, computed, Fragment, PropType } from 'vue'
+import { useContact, useNotification, useStore } from '@/store'
+import { divineHandler } from '@/utils/utils-common'
+import { fetchInvite, fetchCompadre } from '@/components/layer/layer.instance'
+import * as env from '@/interface/instance.resolver'
+
+export default defineComponent({
+    name: 'NextJoinContact',
+    props: {
+        node: { type: Object as PropType<env.SchemaCommunit>, required: true }
+    },
+    setup(props) {
+        return () => <common-element class="n-chunk n-center n-pointer"></common-element>
+    }
+})
+</script>
